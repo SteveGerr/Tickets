6870744f6f9e0930fefe21c204060558f7ed3bdc
SteveGerr <prikolsamara@mail.ru>
2020-10-04 11:11:14 +0400

    Add tickets project

---
+++ src/services/apiService.js
@@ -0,0 +1,45 @@
+
+
+import axios from "axios";
+import config from "../config/apiConfig";
+
+
+/**
+ * /countries - array
+ * /cities - array
+ * /prices/cheap - array
+ */
+
+class Api {
+    constructor(config) {
+        this.url = config.url;
+    }
+
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`)
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return Promise.reject(error);
+        }
+    }
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`)
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return Promise.reject(error);
+        }
+
+    }
+    async prices(params) {
+
+    }
+}
+
+
+const api = new Api(config);
+
+export default api;
